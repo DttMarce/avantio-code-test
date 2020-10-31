2eab76eeda5ac9b36967c06c9ccabd5e312f08f7
Marce <mgalindo@avantio.com>
2020-10-31 23:54:52 +0100

    WOLB-1576 | Fix ConfigWebUsuario

---
+++ back/config/express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const celebrate = require('celebrate');
+const cors = require('cors');
+
+const api = require('../api/routes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/v1/', api);
+app.use(cors());
+app.use(celebrate.errors());
+
+module.exports = app;
