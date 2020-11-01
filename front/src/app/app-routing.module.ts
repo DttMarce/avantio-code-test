cb77d75360e0a5590a2a8522dc5aea31a65800c0
Marce <mgalindo@avantio.com>
2020-11-01 13:06:46 +0100

    Init Front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FeedNewsComponent } from './components/feed-news/feed-news.component';
+
+const routes: Routes = [
+  { path: '', component: FeedNewsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
