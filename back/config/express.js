be8d96dc1c738686e681381bf71ddfc9497840ef
Marce <mgalindo@avantio.com>
2020-11-01 13:06:18 +0100

    Init Front

--- back/config/express.js
+++ back/config/express.js
@@ -4,11 +4,13 @@ const celebrate = require('celebrate');
 const cors = require('cors');
 
 const api = require('../api/routes');
+const accessControl = require('./middleware/access-control');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(accessControl);
 app.use('/v1/', api);
 app.use(cors());
 app.use(celebrate.errors());
